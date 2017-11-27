034fe4454fafbd44d524d88474f5d701873e7859
Plams Li <lishiyuwhu@gmail.com>
2017-11-27 15:29:33 +0800

    2017年11月27日

--- Crowd/crowd_algorithm.py
+++ Crowd/crowd_algorithm.py
@@ -6,8 +6,12 @@
 
 import csv
 import numpy as np
+import pandas as pd
 
-def equal_probability_vote(task_run_info):
+
+info_max = 0
+
+def equal_probability_vote1(task_run_info):
     # 相同task_id的项, 即同一个任务, info取出现次数最高的一个, 次数相同取靠前的, 存入done_data_set.csv
     # 会丢掉最后一行的数据...等等再用pandas重写吧
     with open(task_run_info) as f, open('done_data_set.csv', 'w') as w :
@@ -40,6 +44,24 @@ def equal_probability_vote(task_run_info):
                 infolist = []
                 infolist.append(int(row['info']))
                 lastrow = row
+
+def conv_sparse(num):
+    #类别是从0开始的
+    global  info_max
+    s_list =[0 for n in range(info_max + 1)]
+    s_list[num]=1
+    return s_list
+
+
+def equal_probability_vote(task_run_info):
+    global info_max
+    data = pd.read_csv(task_run_info)
+    data = data.loc[0:10,['task_id', 'info', 'title',' url_b' ,'user_id', 'user_ip']]
+    info_max = data['info'].max(axis=0)
+    data['sparse_info'] = data['info'].map(conv_sparse)
+    # g_data = data.groupby('task_id')
+    g = data.groupby('task_id')
+    print(g)
 
 
 if __name__ == '__main__':
